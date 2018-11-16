b021caa878ddb658a37da375dbb785110bae18c9
Bartłomiej Łazarczyk <lazar81ba@gmail.com>
2018-11-16 18:10:33 +0100

    material add

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -3,14 +3,28 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { MainpageComponent } from './content/mainpage/mainpage.component';
+import {AppRouteModule} from './app-route.module';
+import { AuthorizationComponent } from './content/authorization/authorization.component';
+import { RegistrationComponent } from './content/registration/registration.component';
+import { HeaderComponent } from './header/header.component';
+import { ContentComponent } from './content/content.component';
+import {FormsModule} from '@angular/forms';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainpageComponent,
+    AuthorizationComponent,
+    RegistrationComponent,
+    HeaderComponent,
+    ContentComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRouteModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
