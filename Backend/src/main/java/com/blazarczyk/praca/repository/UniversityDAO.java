b20ea47f827a7f20bb686cae7a1a886997a96a66
Bartłomiej Łazarczyk <lazar81ba@gmail.com>
2018-11-17 19:27:20 +0100

    Implementing FacultyDAO

--- Backend/src/main/java/com/blazarczyk/praca/repository/UniversityDAO.java
+++ Backend/src/main/java/com/blazarczyk/praca/repository/UniversityDAO.java
@@ -9,6 +9,7 @@ import java.util.List;
 
 public interface UniversityDAO extends CrudRepository<University, Long> {
     public University findById(Long id);
+    public University findByName(String name);
     public University findByUsersContaining(User user);
     public List<University> findAllByCountry(Country country);
 }
