4284e50dda236bf9c784a65fa8a9d0dac58a3b0b
Bartłomiej Łazarczyk <lazar81ba@gmail.com>
2018-11-17 20:34:48 +0100

    Implementing Specialization service

---
+++ Backend/src/main/java/com/blazarczyk/praca/service/implementation/SpecializationServiceImpl.java
@@ -0,0 +1,39 @@
+package com.blazarczyk.praca.service.implementation;
+
+import com.blazarczyk.praca.model.Specialization;
+import com.blazarczyk.praca.repository.FacultyDAO;
+import com.blazarczyk.praca.repository.SpecializationDAO;
+import com.blazarczyk.praca.repository.UserDAO;
+import com.blazarczyk.praca.service.SpecializationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class SpecializationServiceImpl implements SpecializationService {
+
+    @Autowired
+    SpecializationDAO specializationDAO;
+
+    @Autowired
+    UserDAO userDAO;
+
+    @Autowired
+    FacultyDAO facultyDAO;
+
+    @Override
+    public List<Specialization> getAllSpecializationsWithName(String name) {
+        return specializationDAO.findAllByName(name);
+    }
+
+    @Override
+    public List<Specialization> getSpecializationsWithinFaculty(long faculty_id) {
+        return specializationDAO.findAllByFacultiesContaining(facultyDAO.findById(faculty_id));
+    }
+
+    @Override
+    public Specialization getSpecializationAttendingByStudent(long user_id) {
+        return specializationDAO.findByUsersContaining(userDAO.findById(user_id));
+    }
+}
