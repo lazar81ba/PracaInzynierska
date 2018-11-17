dc98859b8ecf89e4d7a7e399fd1b9991616267dd
Bartłomiej Łazarczyk <lazar81ba@gmail.com>
2018-11-17 20:17:11 +0100

    Implementing University service

---
+++ Backend/src/main/java/com/blazarczyk/praca/service/UniversityService.java
@@ -0,0 +1,11 @@
+package com.blazarczyk.praca.service;
+
+import com.blazarczyk.praca.model.University;
+
+import java.util.List;
+
+public interface UniversityService {
+    University getUniversityWithName(String name);
+    University getUniversityWithAttendingStudent(long user_id);
+    List<University> getAllUniversitiesInCountry(long country_id);
+}
--- Backend/src/main/java/com/blazarczyk/praca/service/implementation/TagServiceImpl.java
+++ Backend/src/main/java/com/blazarczyk/praca/service/implementation/TagServiceImpl.java
@@ -10,7 +10,7 @@ import java.util.List;
 
 @Service
 public class TagServiceImpl implements TagService {
-    
+
     @Autowired
     TagDAO tagDAO;
 
