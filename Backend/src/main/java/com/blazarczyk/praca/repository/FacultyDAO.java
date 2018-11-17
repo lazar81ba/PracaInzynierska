df6e5349962acffb431d5ec7df0e205ec8b869ae
Bartłomiej Łazarczyk <lazar81ba@gmail.com>
2018-11-17 19:31:47 +0100

    Implementing SpecializationDAO

--- Backend/src/main/java/com/blazarczyk/praca/repository/FacultyDAO.java
+++ Backend/src/main/java/com/blazarczyk/praca/repository/FacultyDAO.java
@@ -1,6 +1,7 @@
 package com.blazarczyk.praca.repository;
 
 import com.blazarczyk.praca.model.Faculty;
+import com.blazarczyk.praca.model.Specialization;
 import com.blazarczyk.praca.model.University;
 import com.blazarczyk.praca.model.User;
 import org.springframework.data.repository.CrudRepository;
@@ -12,4 +13,5 @@ public interface FacultyDAO extends CrudRepository<Faculty, Long> {
     public List<Faculty> findAllByName(String name);
     public List<Faculty> findAllByUniversity(University university);
     public Faculty findByUsersContaining(User user);
+    public List<Faculty> findAllBySpecializationsContaining(Specialization specialization);
 }
